2615b36eb09fe48540d90af596ab31ff7b2a1fc4
linwanqing0613 <l55997140@gmail.com>
2025-02-13 22:39:17 +0800

    Item Service:02-1 unfinished

---
+++ item-service/src/test/java/com/example/item_server/ItemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.item_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
